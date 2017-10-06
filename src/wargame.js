a73e2910a2075594f9a52b60117de1d0d7ed13d4
Leonardo Val <leonardo.val@creatartis.com>
2017-10-06 11:30:15 -0300

    Clone instead of sermat.
    + Using `Sermat.clone` instead of `Sermat.sermat` to copy the game state.

--- src/wargame.js
+++ src/wargame.js
@@ -1,5 +1,5 @@
 /** # Wargame
- * 
+ *
  */
 var Wargame = exports.Wargame = declare(ludorum.Game, {
 	name: 'Wargame',
@@ -7,7 +7,7 @@ var Wargame = exports.Wargame = declare(ludorum.Game, {
 	rounds:10,
 
 	/** ## Constructor and state handling ##########################################################
-	
+
 	The constructor takes the following parameters:
 	*/
 	constructor: function Wargame(params) {
@@ -19,7 +19,7 @@ var Wargame = exports.Wargame = declare(ludorum.Game, {
 			/** + `terrain`: The terrain on which the game is being played.
 			*/
 			.object('terrain')
-			/** + `round`: The current round number. A round is completed after all players have 
+			/** + `round`: The current round number. A round is completed after all players have
 			finished their turns.
 			*/
 			.integer('round', { coerce: true, defaultValue: -1 })
@@ -36,7 +36,7 @@ var Wargame = exports.Wargame = declare(ludorum.Game, {
 			this.nextTurn();
 		}
 	},
-	
+
 	/** Advance the game to the next (or the first) turn.
 	*/
 	nextTurn: function nextTurn() {
@@ -46,11 +46,11 @@ var Wargame = exports.Wargame = declare(ludorum.Game, {
 			this.activePlayers = [this.players[i]]; //FIXME Use Game.activatePlayer.
 			if (!i) {
 				this.nextRound();
-			} 
+			}
 		}
 		return this;
 	},
-	
+
 	nextRound: function nextRound() {
 		this.round++;
 		var armies = this.armies;
@@ -59,28 +59,28 @@ var Wargame = exports.Wargame = declare(ludorum.Game, {
 		}
 		return this;
 	},
-	
+
 	// ## Game ending and scores ###################################################################
-	
+
 	/** An array with the minimum and maximum possible results.
 	 */
 	//resultBounds: [0,750], //TODO Calcular los topes reales.
-	
+
 	/**
 	 * A medida que se destruye por completo a una unidad enemiga, el score incrementa segun el coste de dicha unidad eliminada.
 	 * Diccionario con claves los jugadores y valor su score. Ejemplo {One: 0, Two: 0};
 	 */
 	scores: function scores() {
 		return iterable(this.armies).mapApply(function (name, army) {
-			return [name, army.worth()];			
+			return [name, army.worth()];
 		}).toObject();
 	},
-	
+
 	/**
-	 * returns an object with the game's result if the game is final. 
+	 * returns an object with the game's result if the game is final.
 	 */
 	result: function result() {
-		var player1 = this.players[0], 
+		var player1 = this.players[0],
 			player2 = this.players[1];
 		if (this.armies[player1].isEliminated(this) ||
 				this.armies[player2].isEliminated(this) ||
@@ -91,20 +91,20 @@ var Wargame = exports.Wargame = declare(ludorum.Game, {
 		}
 		return null; // Game continues.
 	},
-	
+
 	/** The definitions of the metagame have to be synchronized with the current game state before
 	any calculation is performed.
 	*/
 	synchronizeMetagame: function synchronizeMetagame() {
 		this.terrain.resetTerrain(this);
 	},
-	
+
 	/** An `activeUnit` is a unit that can take actions in this game state.
 	*/
 	activeUnit: function activeUnit() {
 		return this.__activeUnit__;
 	},
-	
+
 	/** The `moves` method calculates the active player's actions for this game state.
 	*/
 	moves: function moves() {
@@ -115,7 +115,7 @@ var Wargame = exports.Wargame = declare(ludorum.Game, {
 		}
 		return null;
 	},
-	
+
 	/** Executes the given action on this game state. If the action has aleatories, a contingent
 	game state is returned.
 	*/
@@ -128,20 +128,20 @@ var Wargame = exports.Wargame = declare(ludorum.Game, {
 		if (aleatories && !haps) {
 			return new ludorum.Contingent(this, actions, aleatories, update);
 		} else {
-			var nextGame = update ? this : Sermat.sermat(this); //FIXME Sermat.clone
+			var nextGame = update ? this : Sermat.clone(this);
 			action.execute(nextGame, haps);
 			return nextGame.nextTurn();
 		}
 	},
-	
+
 	// ## Serialization ############################################################################
-	
+
 	'static __SERMAT__': {
 		serializer: function serialize_Wargame(obj) {
 			var args = {
-					armies: obj.armies, 
+					armies: obj.armies,
 					terrain: obj.terrain,
-					round: obj.round, 
+					round: obj.round,
 					rounds: obj.rounds
 				};
 			if (obj.__activeUnit__) {
@@ -151,4 +151,3 @@ var Wargame = exports.Wargame = declare(ludorum.Game, {
 		}
 	}
 }); // declare Wargame
-
