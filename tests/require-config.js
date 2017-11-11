4bd040d264ba690137a8aadf715e2062108d5c0a
Rodrigo German <mrodrigo.german@gmail.com>
2017-11-10 21:36:58 -0300

    Actualizado el build

---
+++ tests/require-config.js
@@ -0,0 +1,47 @@
+// Generated code, please do NOT modify.
+(function (config) { "use strict";
+	define([], function () {
+		if (window.__karma__) {
+			config.baseUrl = '/base';
+			for (var p in config.paths) {
+				config.paths[p] = config.paths[p]
+					.replace(/^\.\.\//, '/base/');
+			}
+			config.deps = Object.keys(window.__karma__.files) // Dynamically load all test files
+				.filter(function (file) { // Filter test modules.
+					return /\.test\.js$/.test(file);
+				}).map(function (file) { // Normalize paths to RequireJS module names.
+					return file.replace(/^\/base\/(.*?)\.js$/, '$1');
+				});
+		}
+		require.config(config);
+		console.log("RequireJS configuration: "+ JSON.stringify(config, null, '  '));
+
+		return function (deps, main) {
+			require(deps, function () {
+				var args = Array.prototype.slice.call(arguments);
+				args.forEach(function (module, i) {
+					name = module.__name__ || deps[i];
+					if (window.hasOwnProperty(name)) {
+						console.error("Global `"+ name +"` already defined!");
+					} else {
+						window[name] = module;
+						console.log("Loaded library `"+ deps[i] +"` is available as `window."+
+							name +"`.");
+					}
+				});
+				main.apply(window, args);
+				console.log("Ready.");
+			}, function (err) {
+				console.error(err);
+			});
+		};
+	});
+})({
+	"paths": {
+		"ludorum-wargame": "../build/ludorum-wargame",
+		"creatartis-base": "../node_modules/creatartis-base/build/creatartis-base.min",
+		"sermat": "../node_modules/sermat/build/sermat-umd-min",
+		"ludorum": "../node_modules/ludorum/build/ludorum.min"
+	}
+});
